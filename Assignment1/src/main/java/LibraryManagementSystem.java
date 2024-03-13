5c543135c39c85c64e3198e3530c545107838c9d
noumanghauri <163240790+noumanghauri@users.noreply.github.com>
2024-03-13 14:45:08 +0500

    Add files via upload

---
+++ Assignment1/src/main/java/LibraryManagementSystem.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+public class LibraryManagementSystem{
+    public static void main(String args[]) {
+        System.out.print("A");
+        User ali = new User(11, 121, "Ali");
+    }
+}
