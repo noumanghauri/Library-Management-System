d445f73af15f7a7b83783f12734a0149e01d9c5d
noumanghauri <163240790+noumanghauri@users.noreply.github.com>
2024-03-13 14:47:45 +0500

    Delete Assignment1 directory

--- Assignment1/src/main/java/LibraryManagementSystem.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.awt.*;
-import java.util.Scanner;
-public class LibraryManagementSystem{
-    public static void main(String args[]) {
-        System.out.print("A");
-        User ali = new User(11, 121, "Ali");
-    }
-}
